fdfe7e8cb7d9574d91062de95b7dd4e943422a87
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-03-12 19:06:56 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,6 +17,7 @@ require 'pry'
 def remove_strawberry(contacts)
 contacts.each do |person, hash_details|
   hash_details.each do |attribute, data|
+    if attribute == :favorite_ice_cream_flavors
   end
   end
 end
